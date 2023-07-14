8d73c9a3fcdcf857960f5bee58e83fb5d7a3e931
Hui Tong <937013596@qq.com>
2023-07-14 11:09:22 +0800

    [C] IO库 open和close使用
     - r_close到restart.h
     - open函数的访问模式和文件权限

--- c/unix/restart.c
+++ c/unix/restart.c
@@ -1,6 +1,9 @@
 #include <errno.h>
+#include <limits.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#define BLKSIZE PIPE_BUF
 
 pid_t r_wait(int *stat_local) {
   int retval;
@@ -28,6 +31,33 @@ ssize_t r_write(int fd, void *buf, size_t size) {
     if ((byteswritten == -1) && (errno == EINTR)) return -1;
     if (byteswritten == -1) byteswritten = 0;
     totalbytes += byteswritten;
+  }
+  return totalbytes;
+}
+
+int r_close(int fd) {
+  int retval;
+  while (retval = close(fd), retval == -1 && errno == EINTR)
+    ;
+  return retval;
+}
+
+int readwrite(int fromfd, int tofd) {
+  char buf[BLKSIZE];
+  int bytesread;
+
+  if ((bytesread = r_read(fromfd, buf, BLKSIZE)) < 0) return -1;
+  if (bytesread == 0) return 0;
+  if (r_write(tofd, buf, bytesread) < 0) return -1;
+  return bytesread;
+}
+
+int copyfile(int fromfd, int tofd) {
+  int bytesread;
+  int totalbytes = 0;
+
+  while ((bytesread = readwrite(fromfd, tofd)) > 0) {
+    totalbytes += bytesread;
   }
   return totalbytes;
 }
--- c/unix/restart.h
+++ c/unix/restart.h
@@ -2,3 +2,6 @@
 int r_wait(int *stat_local);
 ssize_t r_read(int fd, void *buf, size_t size);
 ssize_t r_write(int fd, void *buf, size_t size);
+int r_close(int fd);
+int readwrite(int fromfd, int tofd);
+int copyfile(int fromfd, int tofd);
